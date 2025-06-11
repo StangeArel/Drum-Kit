f7152c1c8171e6f18cdb3a7c560a428f961f0826
Valeriya Stange <valeriya.stange@gmail.com>
2025-06-11 12:11:11 +0200

    ✅ Finish working Drum Kit project

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-console.log("-".repeat(20));
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
     if (!audio) return;       // --> stop the function from running all together 
     audio.currentTime = 0;    // --> rewind to the start
-    audio.play();
-    console.log(key);
+    audio.play().catch(err => {
+        console.error("Fehler beim Abspielen:", err);
+    });
+    key.classList.add('playing');
+};
 
-});
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;  //--> skip it if it´s not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
